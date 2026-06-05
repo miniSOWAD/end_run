d16ef2e3288d85d554017539b326649bb131fc35
Md Mahruf Alam <baisakh2015@gmail.com>
2026-06-05 09:29:44 +0600

    major update 2

---
+++ lib/levels/all_levels.dart
@@ -0,0 +1,24 @@
+import 'maze_level.dart';
+import 'level_01.dart';
+import 'level_02.dart';
+import 'level_03.dart';
+import 'level_04.dart';
+import 'level_05.dart';
+import 'level_06.dart';
+import 'level_07.dart';
+import 'level_08.dart';
+import 'level_09.dart';
+import 'level_10.dart';
+
+final List<MazeLevel> allLevels = [
+  buildLevel01(),
+  buildLevel02(),
+  buildLevel03(),
+  buildLevel04(),
+  buildLevel05(),
+  buildLevel06(),
+  buildLevel07(),
+  buildLevel08(),
+  buildLevel09(),
+  buildLevel10(),
+];
